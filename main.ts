df3fda31d41ba0dd32604955de34fc2193b5e0e8
Pablo-Carreras <pablo.carreras-canton@mths.ca>
2024-10-01 11:16:09 -0400

    made the code

--- main.ts
+++ main.ts
@@ -1,8 +1,24 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: Mr. Coxall
- * Created on: Sep 2020
- * This program ...
+ * Created by: Pablo
+ * Created on: Sep 2024
+ * This program adds cookies and restarts the counter 
 */
 
-basic.showString('Hello, World!')
+let cookieNumber: number
+cookieNumber = 0
+
+basic.clearScreen()
+basic.showIcon(IconNames.Happy)
+
+//add 1 to the count 
+input.onButtonPressed(Button.A, function () {
+    cookieNumber = (cookieNumber + 1)
+    basic.showNumber(cookieNumber)
+})
+
+//resets the count 
+input.onButtonPressed(Button.B, function () {
+    cookieNumber = (cookieNumber - cookieNumber)
+    basic.showNumber(cookieNumber)
+})
